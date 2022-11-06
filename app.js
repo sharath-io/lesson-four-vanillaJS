f6464172b33985672f95f14bdfef5651c8b67d1d
Sharath <sharath.esr@gmail.com>
2022-11-06 15:05:39 +0530

    using external js file

---
+++ app.js
@@ -0,0 +1,9 @@
+var userName = prompt('Enter your username :');
+alert('Welcome '+ userName);
+
+//or
+// var userName = prompt('Enter your username :');
+// var welcomeMessage = 'Welcome '+ userName;
+// alert(welcomeMessage);
+
+
